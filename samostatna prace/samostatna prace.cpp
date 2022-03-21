2766cd1341b988fe52c026c949f6d0a66da75469
matous.bien.020 <matous.bien.020@B000-10>
2022-03-21 13:24:24 +0100

    Přidejte soubory projektu.

---
+++ samostatna prace/samostatna prace.cpp
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+
+int main()
+{
+    int cislo = 6;
+    int soucetsudy = 0;
+    int soucetlichy = 0;
+    for (int i = 1; i <= cislo; i++) {
+        if (i % 2 == 0) soucetsudy += i;
+        else if (i % 2 != 0) soucetlichy += i;
+
+    }
+    printf("sudy soucet: %d\n", soucetsudy);
+    printf("lichy soucet: %d\n", soucetlichy);
+}
